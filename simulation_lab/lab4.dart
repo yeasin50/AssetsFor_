5f3338a6676f5e0a7e9b8d8056eaaa85feab0c74
yeasin50 <yeasinsheikh50@gmail.com>
2022-09-22 17:21:17 +0600

    added-simulation-lab4

---
+++ simulation_lab/lab4.dart
@@ -0,0 +1,36 @@
+import 'dart:io';
+import 'dart:math' as math;
+
+void area(double radius) {
+  final result = math.pi * math.pow(radius, 2);
+
+  print("A=${result.toStringAsFixed(4)}");
+}
+
+BigInt sqByN(int n) => BigInt.from(math.pow(n, n * 2));
+
+void powerTask(int start, int end) {
+  for (int i = start; i <= end; i++) {
+    print("number: $i value:${sqByN(i)}");
+  }
+}
+
+///* number: 2 value: 16
+/// user i = 2;
+///  result  = pow(2,2*2) => 16
+///
+
+int readInput() => int.tryParse(stdin.readLineSync() ?? "") ?? 0;
+
+void edgeBase(int a, int c) {
+  final x = math.sqrt(math.pow(a, 2) - math.pow(c, 2));
+  print("base $x");
+}
+
+void main(List<String> args) {
+  stdout.write("Enter a: ");
+  int a = readInput();
+  stdout.write("Enter c: ");
+  int c = readInput();
+  edgeBase(a, c);
+}
