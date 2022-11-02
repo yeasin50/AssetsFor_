1728165bba0d97291ad4bdcd7c768a290442e124
yeasin50 <yeasinsheikh50@gmail.com>
2022-11-02 16:02:09 +0600

    re-org

--- simulation_lab/lab3.dart
+++ simulation_lab/lab3.dart
@@ -1,8 +1,8 @@
 import 'dart:io';
 
 t1() {
-  for (int i = 0; i <= 5; i++) {
-    for (int j = 0; j <= 5; j++) {
+  for (int i = 0; i < 5; i++) {
+    for (int j = 0; j < 5; j++) {
       stdout.write(
         "* ",
       );
@@ -19,24 +19,24 @@ leapYearCheck(int year) {
 }
 
 shape2(int n) {
-  int value = 1;
+ 
   for (int i = 1; i <= n; i++) {
     for (int j = 1; j <= i; j++) {
-      stdout.write("${value++} ");
+      stdout.write("${i} ");
     }
     print("");
   }
 }
 
-//    1 
-//   2 3 
-//  4 5 6 
-// 7 8 9 10 
-  
+//    1
+//   2 3
+//  4 5 6
+// 7 8 9 10
+
 shape3(int n) {
   int value = 1;
   for (int i = 1; i <= n; i++) {
-    for (int j =i; j <  n ; j++) {
+    for (int j = i; j < n; j++) {
       stderr.write(" ");
     }
     for (int j = 0; j < i; j++) {
@@ -48,5 +48,5 @@ shape3(int n) {
 
 void main(List<String> args) {
   int n = int.tryParse(stdin.readLineSync() ?? "") ?? 0;
-  shape3(n);
+  shape2(n);
 }
